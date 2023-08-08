ada9fc3031fc800e7548d5dc2a845d58b5ef2860
gkusumbe7 <gkusumbe7@gmail.com>
2023-08-09 00:04:59 +0530

    print the pattern-1

---
+++ New folder/page1(02).java
@@ -0,0 +1,11 @@
+public class page1(02) {
+    public  static void main(String args[])
+    {
+        System.out.println("Hello");
+        System.out.println("World.");
+        System.out.println("I");
+        System.out.println("am");
+        System.out.println("here.");
+
+    }
+}
