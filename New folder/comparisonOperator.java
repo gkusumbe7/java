a39ed12cce1c39c6620c9a1de86a1103c01f542e
gkusumbe7 <gkusumbe7@gmail.com>
2023-08-16 20:24:12 +0530

    added comparision operator

---
+++ New folder/comparisonOperator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class comparisonOperator {
+    
+public  static void main(String args[])
+    {
+        System.out.println(15 > 10 );
+        System.out.println(5 < 2 );
+        System.out.println(10 <= 15 );
+        System.out.println(20 <= 20);
+        System.out.println(40 >= 50 );
+        System.out.println(10 == 10 );
+        System.out.println(10 != 20 );
+    }
+
+}
